3a8e4a59311d16ca6408c10820a16dc5265c70e3
vanshbhojani <vanshbhojani3@gmail.com>
2026-09-22 14:43:21 +0530

    ..

--- Numpy/ASSESSMENT numpy/ASSESSMENT 1 .py
+++ Numpy/ASSESSMENT numpy/ASSESSMENT 1 .py
@@ -1,4 +1,43 @@
 import pandas as pd
 import numpy as np
 
-f1 = pd.read_excel("Numpy\ASSESSMENT numpy\banklist.csv")
+
+# 1) Import pandas and read in the banklist.csv file into a dataframe called banks.
+
+f1 = pd.read_csv("Numpy/ASSESSMENT numpy/banklist.csv")
+print(f1)
+
+# 2) Show the head of the dataframe.
+
+print(f1.head())
+
+print(f1.head(5))
+
+# 3) What are the column names?
+
+print(f1.columns)
+
+# 4) How many States (ST) are represented in this data set?
+
+print(len(f1['ST']))
+
+# 5) Get a list or array of all the states in the data set.
+
+print(f1['ST'].unique())
+
+# 6) What are the top 5 states with the most failed banks?
+
+print(f1.groupby('ST').count())
+
+# 7) What are the top 5 acquiring institutions?
+
+print(f1.groupby('Acquiring Institution').count())
+
+# 8) How many banks has the State Bank of Texas acquired? How many of them were actually in Texas?
+
+print(f1.loc[f1['ST'] == 'Texas'])
+
+# 9) What is the most common city in California for a bank to fail in?
+
+print(f1.groupby(['ST', 'City']).count())
+
---
+++ SQL/New folder/session1.sql
@@ -0,0 +1,15 @@
+create database music_streaming_db;
+use music_streaming_db;
+
+CREATE database food_delivery_db;
+show databases;
+use food_delivery_db;
+
+-- Feature / Use Case	MySQL	PostgreSQL
+-- 1. Main use --> mysql-->	Commonly used for web applications and transactional workloads	
+-- 			   -->PostgreSQL-->Commonly used for complex applications and analytical/data-intensive workloads
+
+-- 2. Advanced featuresm --->mysql--->Strong support for standard relational database features and easy-to-use replication options
+-- 	                     ---)PostgreSQL--->Strong support for advanced SQL, complex queries, custom data types, and extensibility
+-- 3. Data types / extensibility --->mysql--->Supports common relational data types and JSON
+-- 								 --->PostgreSQL--->	Has extensive data-type support, including JSON/JSONB, arrays, and user-defined types
